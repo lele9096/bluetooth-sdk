a1048b839fdcb3ba3725695cfb1cac0613b2838e
LELE <le.yu@chargedot.com>
2018-12-13 14:24:58 +0800

    add CDbleSDK.framework

---
+++ CDBleSDK.framework/Headers/CDBleSetResult.h
@@ -0,0 +1,19 @@
+//
+//  CDBleSetResult.h
+//  LZAPI
+//
+//  Created by LELE on 2018/11/20.
+//  Copyright © 2018年 rect. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/**
+ *  设置类型操作结果
+ */
+typedef NS_ENUM(NSInteger,CDBleSetResultType){
+    CDBleSetResultTypeSuccess =  0, 
+    CDBleSetResultTypeFailue  =  1
+};
+@interface CDBleSetResult : NSObject
+@property(nonatomic,assign)CDBleSetResultType resultType;
+@end
